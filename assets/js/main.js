d0ba4c5e7c0f9ba7e7bcae62b8b4a0349a47a1fd
Strangebrewer <BKAShambala@gmail.com>
2018-03-27 00:23:04 -0600

    set up OpenWeatherMap.org api instead of Weather Underground

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,35 +7,108 @@ $('.autoplay').slick({
   appendDots: $(".slider-frame")
 });
 
+function degreesToCardinal(deg) {
+  if (deg>11.25 && deg<33.75){
+    return "NNE";
+  }else if (deg>33.75 && deg<56.25){
+    return "NE";
+  }else if (deg>56.25 && deg<78.75){
+    return "ENE";
+  }else if (deg>78.75 && deg<101.25){
+    return "E";
+  }else if (deg>101.25 && deg<123.75){
+    return "ESE";
+  }else if (deg>123.75 && deg<146.25){
+    return "SE";
+  }else if (deg>146.25 && deg<168.75){
+    return "SSE";
+  }else if (deg>168.75 && deg<191.25){
+    return "S";
+  }else if (deg>191.25 && deg<213.75){
+    return "SSW";
+  }else if (deg>213.75 && deg<236.25){
+    return "SW";
+  }else if (deg>236.25 && deg<258.75){
+    return "WSW";
+  }else if (deg>258.75 && deg<281.25){
+    return "W";
+  }else if (deg>281.25 && deg<303.75){
+    return "WNW";
+  }else if (deg>303.75 && deg<326.25){
+    return "NW";
+  }else if (deg>326.25 && deg<348.75){
+    return "NNW";
+  }else{
+    return "N"; 
+  }
+}
+
+var apiKey = "4ecdc01b5584988caec51ee700a06511";
+var proxy = "https://obscure-gorge-69381.herokuapp.com/";
+
+
 $.ajax({
   async: true,
   crossDomain: true,
-  url: "https://obscure-gorge-69381.herokuapp.com/http://api.wunderground.com/api/628f83f5e25fe2dc/conditions/q/UT/Bountiful.json",
+  url: "https://api.openweathermap.org/data/2.5/weather?zip=84011,us&units=imperial&APPID=" + apiKey,
   method: "GET"
 }).then(response => {
-  var weather = response.current_observation;
+  console.log(response);
+  var degrees = degreesToCardinal(response.wind.deg);
+  var convertedTemp = Math.round(response.main.temp);
+  var convertedSpeed = Math.round(response.wind.speed);
   var weatherDiv = $("<div>");
-  var loc = $("<p>Weather in " + weather.display_location.full + ": </p>");
-  var image = $("<img src='" + weather.icon_url + "'>");
-  var cond = $("<p>" + weather.weather + "</p>");
-  var temp = $("<p>" + weather.temp_f + " F" + "</p>");
-  var wind = $("<p>Wind: " + weather.wind_dir + " " + weather.wind_mph + "mph</p>");
-  var logo = $("<a href='https://www.wunderground.com/?apiref=abb3d872dc5ca35d' class='wu-link'><img src='assets/images/wundergroundLogo_4c_horz.png'></a>");
+  var loc = $("<p>Local Weather: </p>");
+  var image = $("<img src='http://openweathermap.org/img/w/" + response.weather[0].icon + ".png'>");
+  var cond = $("<p>" + response.weather[0].main + "</p>");
+  var temp = $("<p>" + convertedTemp + " F" + "</p>");
+  var wind = $("<p>Wind: " + degrees + " " + convertedSpeed + "mph</p>");
+  var attribution = $("<p class='weather-link'>Weather data provided by <a href='https://openweathermap.org' target='_blank'>OpenWeatherMap.org</a>");
   weatherDiv.addClass("weather-div");
   image.addClass("weather-img");
   cond.addClass("weather-cond");
   temp.addClass("weather-temp");
   wind.addClass("weather-wind");
   loc.addClass("weather-loc");
-  logo.addClass("weather-logo");
   weatherDiv.append(loc);
   weatherDiv.append(image);
   weatherDiv.append(cond);
   weatherDiv.append(wind);
   weatherDiv.append(temp);
   $("#weather-display").append(weatherDiv);
-  $("#weather-display").append(logo);
-})
+  $("#weather-display").append(attribution);
+});
+
+
+// $.ajax({
+//   async: true,
+//   crossDomain: true,
+//   url: "https://obscure-gorge-69381.herokuapp.com/http://api.wunderground.com/api/628f83f5e25fe2dc/conditions/q/UT/Bountiful.json",
+//   method: "GET"
+// }).then(response => {
+//   var weather = response.current_observation;
+//   var weatherDiv = $("<div>");
+//   var loc = $("<p>Weather in " + weather.display_location.full + ": </p>");
+//   var image = $("<img src='" + weather.icon_url + "'>");
+//   var cond = $("<p>" + weather.weather + "</p>");
+//   var temp = $("<p>" + weather.temp_f + " F" + "</p>");
+//   var wind = $("<p>Wind: " + weather.wind_dir + " " + weather.wind_mph + "mph</p>");
+//   var logo = $("<a href='https://www.wunderground.com/?apiref=abb3d872dc5ca35d' class='wu-link'><img src='assets/images/wundergroundLogo_4c_horz.png'></a>");
+//   weatherDiv.addClass("weather-div");
+//   image.addClass("weather-img");
+//   cond.addClass("weather-cond");
+//   temp.addClass("weather-temp");
+//   wind.addClass("weather-wind");
+//   loc.addClass("weather-loc");
+//   logo.addClass("weather-logo");
+//   weatherDiv.append(loc);
+//   weatherDiv.append(image);
+//   weatherDiv.append(cond);
+//   weatherDiv.append(wind);
+//   weatherDiv.append(temp);
+//   $("#weather-display").append(weatherDiv);
+//   $("#weather-display").append(logo);
+// })
 
 // hamburger menu toggle
 $(".cross").hide();
